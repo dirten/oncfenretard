9d1f1d9d773626271558589e56197becf5f279c7
Adil Hemache <adilhemache@gmail.com>
2017-10-14 18:22:40 +0100

    add caching to stations endpoint

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const moment = require('moment')
+const NodeCache = require( "node-cache" )
 const services = require('./services')
 
 const app = express()
+const cache = new NodeCache({
+    stdTTL: 24 * 60 * 60 // 24 hours time-to-live
+})
 
 app.use(bodyParser.json())
 
 app.get('/stations', (req, res) => {
-    services.getStations().then(stations => res.json(stations))
+    const stations = cache.get('stations')
+    if (stations !== undefined) {
+        return res.json(stations)
+    }
+    
+    services.getStations().then(stations => {
+        cache.set('stations', stations)
+        res.json(stations)
+    })
 })
 
 app.get('/times', (req, res) => {
