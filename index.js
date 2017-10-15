6e6aaf8c974b9fa7b34b4986f44bed1248d2f3e5
Adil Hemache <adilhemache@gmail.com>
2017-10-15 19:36:24 +0100

    fix port binding

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.get('/times', (req, res) => {
         .then(times => res.json(times))
 })
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
 })
